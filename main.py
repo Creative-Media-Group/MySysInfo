8c68fcad8edf2dadc419b314c7a68d11b26eeebd
tct123 <42028373+tct123@users.noreply.github.com>
2024-05-16 10:10:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,28 @@ from mylocale.TR import tr
 lang = locale.getlocale()
 lang, _ = lang
 translation = "assets/translation/localisation.csv"
+with open("README.md", "r") as readme:
+    readme = readme.read()
+with open("LICENSE", "r") as l:
+    applicense = l.read()
+
 
 def main(page: ft.Page):
+    license_dlg = ft.AlertDialog(title=ft.Text(applicense))
+    readme_dlg = ft.AlertDialog(title=ft.Text(readme))
+
+    def open_license_dlg(e):
+        license_dlg.open = True
+
+    def open_readme_dlg(e):
+        readme_dlg.open = True
+
     page.adaptive = True
     page.appbar = ft.AppBar(title=ft.Text("MySysInfo"))
-    page.add(ft.SafeArea(ft.Text(tr(csv_file=translation,target_key="HELLOWORLD"))))
+    page.floating_action_button = ft.FloatingActionButton(
+        icon=ft.icons.INFO, on_click=lambda e: self.open_readme_dlg(e)
+    )
+    page.add(ft.SafeArea(ft.Text(tr(csv_file=translation, target_key="HELLOWORLD"))))
 
 
 ft.app(main)
